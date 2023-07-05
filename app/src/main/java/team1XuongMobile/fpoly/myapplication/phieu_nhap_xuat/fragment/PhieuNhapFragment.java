798780b4674bb6f7dd6ce21e7ac08173a79663fd
haihuy262 <dolcegabana0206@gmail.com>
2023-07-06 00:14:11 +0700

    fragment chi tiet

--- app/src/main/java/team1XuongMobile/fpoly/myapplication/phieu_nhap_xuat/fragment/PhieuNhapFragment.java
+++ app/src/main/java/team1XuongMobile/fpoly/myapplication/phieu_nhap_xuat/fragment/PhieuNhapFragment.java
@@ -1,9 +1,7 @@
 package team1XuongMobile.fpoly.myapplication.phieu_nhap_xuat.fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
