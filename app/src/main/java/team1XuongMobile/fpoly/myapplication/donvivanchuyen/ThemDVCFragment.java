8066d158e0b8d0eb606defa6b8ec5440247738e2
Louis <thang.dev03@gmail.com>
2023-07-07 05:32:57 +0700

    chuc nang them sua xoa van chuyen

--- app/src/main/java/team1XuongMobile/fpoly/myapplication/donvivanchuyen/ThemDVCFragment.java
+++ app/src/main/java/team1XuongMobile/fpoly/myapplication/donvivanchuyen/ThemDVCFragment.java
@@ -1,65 +1,117 @@
 package team1XuongMobile.fpoly.myapplication.donvivanchuyen;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
 import team1XuongMobile.fpoly.myapplication.R;
+import team1XuongMobile.fpoly.myapplication.databinding.FragmentThemDVCBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ThemDVCFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ThemDVCFragment extends Fragment {
+    private FragmentThemDVCBinding binding;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+    private FirebaseUser firebaseUser;
+    private String ten = "", dia_chi = "", sdt = "";
+    private final String TAG = "ThemDVCFragment";
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ThemDVCFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ThemDVCFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ThemDVCFragment newInstance(String param1, String param2) {
-        ThemDVCFragment fragment = new ThemDVCFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = FragmentThemDVCBinding.inflate(inflater, container, false);
+        progressDialog = new ProgressDialog(getContext());
+        firebaseAuth = FirebaseAuth.getInstance();
+        listener();
+
+
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_them_d_v_c, container, false);
+        return binding.getRoot();
     }
-}  
+    public void listener(){
+        binding.buttonHoanTat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validateDataInput();
+
+            }
+        });
+        binding.buttonTroLai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+    }
+
+    public void validateDataInput() {
+        ten = binding.edTenDVC.getText().toString().trim();
+        dia_chi = binding.edDiaChiDVC.getText().toString().trim();
+        sdt = binding.edSoDienThoaiDVC.getText().toString().trim();
+        if (ten.isEmpty() || dia_chi.isEmpty() || sdt.isEmpty()) {
+            Toast.makeText(getActivity(), "Vui lòng nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+        }else {
+            luuDuLieuVaoFireBase();
+
+        }
+    }
+    public void luuDuLieuVaoFireBase(){
+        progressDialog.setTitle("Đag lưu...");
+        progressDialog.show();
+        long timestamp = System.currentTimeMillis();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        HashMap<String,Object> hashMap = new HashMap<>();
+        hashMap.put("id_don_vi_vc","" + timestamp);
+        hashMap.put("ten","" + ten);
+        hashMap.put("dia_chi","" + dia_chi);
+        hashMap.put("hotline","" + sdt);
+        hashMap.put("timestamp", timestamp);
+        hashMap.put("uid",firebaseUser.getUid());
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("don_vi_vc");
+        ref.child(""+timestamp).setValue(hashMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        progressDialog.dismiss();
+                        Toast.makeText(getContext(), "Thêm mới thành công", Toast.LENGTH_SHORT).show();
+                        getActivity().getSupportFragmentManager().popBackStack();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Toast.makeText(getContext(), "Thêm mới thất bại", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, "onFailure: Lỗi vì"+e.getMessage());
+                    }
+                });
+
+
+    }
+}
