de04b6c5c5059b71dbf1db6668804ff280798240
Louis hoàng <126375932+thang236@users.noreply.github.com>
2023-07-18 01:02:53 +0700

    okeee

--- app/src/main/java/team1XuongMobile/fpoly/myapplication/nhacungcap/NhaCungCapFragment.java
+++ app/src/main/java/team1XuongMobile/fpoly/myapplication/nhacungcap/NhaCungCapFragment.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -24,10 +25,13 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
+import team1XuongMobile.fpoly.myapplication.Adapter.LoaiSanPhamAdapter;
+import team1XuongMobile.fpoly.myapplication.Model.LoaiSanPham;
 import team1XuongMobile.fpoly.myapplication.R;
 import team1XuongMobile.fpoly.myapplication.databinding.FragmentDanhSachNCCBinding;
 import team1XuongMobile.fpoly.myapplication.databinding.FragmentSuaNCCBinding;
@@ -60,7 +64,8 @@ public class NhaCungCapFragment extends Fragment implements NhaCungCapAdapter.ch
         nhaCungCapModelArrayList = new ArrayList<>();
         chucNangInterfaceNhaCungCap = this;
         laydulieudangnhap();
-        loadDataFirebase();
+//        loadDataFirebase();
+        loadDuLieuTuFirebase();
 
         listener();
         return binding.getRoot();
@@ -96,25 +101,77 @@ public class NhaCungCapFragment extends Fragment implements NhaCungCapAdapter.ch
         });
 
     }
-    private void loadDataFirebase() {
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("nha_cung_cap");
-        ref.orderByChild("kh").equalTo(kh)
-        .addValueEventListener(new ValueEventListener() {
+//    private void loadDataFirebase() {
+//        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("nha_cung_cap");
+//        ref.orderByChild("kh").equalTo(kh)
+//        .addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                nhaCungCapModelArrayList.clear();
+//                Log.d(TAG, "onDataChange: "+ kh);
+//                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+//                    NhaCungCapModel nhaCungCapModel = dataSnapshot.getValue(NhaCungCapModel.class);
+//                    nhaCungCapModelArrayList.add(nhaCungCapModel);
+//                    Log.d(TAG, "onDataChange: " + nhaCungCapModelArrayList.size());
+//                }
+//                adapter = new NhaCungCapAdapter(nhaCungCapModelArrayList,getContext(),chucNangInterfaceNhaCungCap);
+//                binding.rcvDanhSachNCC.setAdapter(adapter);
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//                Log.d(TAG, "onCancelled: loi load du lieu tu firebase:"+ error.getMessage());
+//            }
+//        });
+//    }
+    private void loadDuLieuTuFirebase() {
+        nhaCungCapModelArrayList = new ArrayList<>();
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser == null) {
+            // User not logged in, handle the case as needed
+            return;
+        }
+
+        String uid = firebaseUser.getUid();
+
+        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("Accounts").child(uid);
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                nhaCungCapModelArrayList.clear();
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                    NhaCungCapModel nhaCungCapModel = dataSnapshot.getValue(NhaCungCapModel.class);
-                    nhaCungCapModelArrayList.add(nhaCungCapModel);
-                }
-                adapter = new NhaCungCapAdapter(nhaCungCapModelArrayList,getContext(),chucNangInterfaceNhaCungCap);
-                binding.rcvDanhSachNCC.setAdapter(adapter);
+                String kh = ""+snapshot.child("kh").getValue(String.class);
+                if (kh != null) {
+                    DatabaseReference nhaccref = FirebaseDatabase.getInstance().getReference("nha_cung_cap");
+                    Query query = nhaccref.orderByChild("kh").equalTo(kh);
 
+                    query.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            nhaCungCapModelArrayList.clear();
+                            for (DataSnapshot dslsp : snapshot.getChildren()) {
+                                NhaCungCapModel themncc = dslsp.getValue(NhaCungCapModel.class);
+                                if (themncc != null) {
+                                    nhaCungCapModelArrayList.add(themncc);
+                                }
+                            }
+
+                            NhaCungCapAdapter nhaCungCapAdapter = new NhaCungCapAdapter(nhaCungCapModelArrayList, getContext(), chucNangInterfaceNhaCungCap);
+                            binding.rcvDanhSachNCC.setLayoutManager(new LinearLayoutManager(getContext()));
+                            binding.rcvDanhSachNCC.setAdapter(nhaCungCapAdapter);
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                            Toast.makeText(getContext(), "Không thêm được dữ liệu lên Firebase", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                Log.d(TAG, "onCancelled: loi load du lieu tu firebase:"+ error.getMessage());
+                Toast.makeText(getContext(), "Không thêm được dữ liệu lên Firebase", Toast.LENGTH_SHORT).show();
             }
         });
     }
